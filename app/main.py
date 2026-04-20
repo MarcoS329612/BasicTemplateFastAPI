19bf836a015e1cba9297914f09ce9e24d702b6f5
MarcoS329612 <a329612@uach.mx>
2026-04-19 20:56:50 -0600

    install uv

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from config.db import create_db_and_tables
 from routers.customer import customer
 
-app = FastAPI(lifespan=create_db_and_tables)
-app.include_router(customer)
+# app = FastAPI(lifespan=create_db_and_tables)
+# app.include_router(customer)
 
 
 @app.get("/")
